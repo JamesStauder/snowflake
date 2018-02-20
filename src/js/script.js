9586a677d9616ebe40c2860fb8b400bbbbd33d03
Silent Accordion <bradford.knowlton@umconnect.umt.edu>
2018-02-20 15:02:01 -0700

    Google Maps and Sliders

--- src/js/script.js
+++ src/js/script.js
@@ -11,4 +11,25 @@ $(function () {
 	// $("#ex6").on("slide", function(slideEvt) {
 	// 	$("#ex6SliderVal").text(slideEvt.value);
 	// });
+
+
 })
+
+function initMap() {
+        var greenland = {lat: 71.7069, lng: -42.6043};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 3,
+          center: greenland,
+          zoomControl: true,
+		  mapTypeControl: false,
+		  scaleControl: false,
+		  streetViewControl: false,
+		  rotateControl: false,
+		  fullscreenControl: false
+
+        });
+        // var marker = new google.maps.Marker({
+        //   position: uluru,
+        //   map: map
+        // });
+      }
