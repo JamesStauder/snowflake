63c75e4ac7bbd315d7b6916ca611ce9b77b2fd17
Silent Accordion <bradford.knowlton@umconnect.umt.edu>
2018-02-20 13:41:07 -0700

    Initial Commit

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+$(function () {
+  	$('[data-toggle="tooltip"]').tooltip();
+
+  	// With JQuery
+	// $('#ex1').slider({
+	// 	formatter: function(value) {
+	// 		return 'Current value: ' + value;
+	// 	}
+	// });
+
+	// $("#ex6").on("slide", function(slideEvt) {
+	// 	$("#ex6SliderVal").text(slideEvt.value);
+	// });
+})
